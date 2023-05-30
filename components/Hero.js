202a5de97531f33839d21ae0e0645b7956ccd01a
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-30 12:28:39 +0600

    next font

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,8 @@
-
 export default function Hero() {
   return (
-    <div className="bg-[url('../public/bg.png')] h-[440px]">
-        
-
-        
+    <div className="h-[440px] bg-gr">
+      <h1 className="text-white text-[36px] font-[700] pl-[200px] pt-72">Computer Engineering</h1>
+      <p className="text-white pl-[200px] ">142,765 Computer Engineers follow this</p>
     </div>
-  )
+  );
 }
