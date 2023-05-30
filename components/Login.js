380dc0f8eb6a2017738ecc2ba95036c9637d33c0
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-30 22:19:03 +0600

    login route

--- components/Login.js
+++ components/Login.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { AiOutlineClose } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { IoEyeOutline } from "react-icons/io5";
 import { RiFacebookCircleFill } from "react-icons/ri";
@@ -8,7 +9,7 @@ export default function Login() {
   return (
     <div className="bg-gray-400 flex justify-center p-52 ">
       <div className="w-[736px] h-[530px] bg-white rounded-[8px]">
-        <div className="flex justify-center bg-[#EFFFF4] py-[17px] px-0 rounded-[8px]">
+        <div className="hidden md:flex justify-center bg-[#EFFFF4] py-[17px] px-0 rounded-[8px]">
           <p className="text-[14px] text-[#008A45] font-[500]">
             Let&apos;s learn, share & inspire each other with our passion for
             computer engineering. Sign up now 🤘🏼
@@ -17,10 +18,15 @@ export default function Login() {
 
         <div className="my-6 px-[36px] flex justify-between items-center text-[14px] ">
           <h2 className="text-[24px] font-[700]">Create Account</h2>
-          <h4 className="flex items-center gap-x-1 ">
+          <h4 className="hidden md:flex items-center gap-x-1 ">
             <span>Already have an account? </span>
             <button className="text-[#2F6CE5] font-[600]">Sign In</button>
           </h4>
+          <button className="h-7 w-7 rounded-full bg-gray-600 text-white flex justify-center items-center">
+            <span>
+              <AiOutlineClose />
+            </span>
+          </button>
         </div>
 
         <div className="px-[36px] flex">
@@ -52,7 +58,7 @@ export default function Login() {
                   className="bg-[#F7F8FA] px-3 py-[11px] border w-[320px] placeholder-[#8A8A8A] font-[500]"
                 />
                 <span className="absolute top-[13px] right-8">
-                  <IoEyeOutline className="text-black/[.5]" size={20}/>
+                  <IoEyeOutline className="text-black/[.5]" size={20} />
                 </span>
               </div>
               <div>
@@ -62,12 +68,18 @@ export default function Login() {
                   className="bg-[#F7F8FA] px-3 py-[11px] border-l border-r border-b rounded w-[320px] placeholder-[#8A8A8A] font-[500]"
                 />
               </div>
-              <input
+              {/* <input
                 type="button"
                 value="Create Account"
                 className="bg-[#2F6CE5] py-3 rounded-full text-white w-[320px] mt-[19px] text-[14px]"
-              />
+              /> */}
             </form>
+            <div className="flex items-center justify-between mt-[19px]">
+              <button className="w-[150px] bg-[#2F6CE5] py-3 rounded-full text-white md:w-[320px]  text-[14px]">
+                Create Account
+              </button>
+              <button className="border-b border-[#495057] text-[#495057] font-[500]">or, Sign In</button>
+            </div>
             <div className="mt-5">
               <div>
                 <button className="flex items-center gap-x-2 py-[11px] border rounded w-[320px] justify-center my-2 text-[14px]">
@@ -90,7 +102,7 @@ export default function Login() {
               </div>
             </div>
           </div>
-          <div>
+          <div className="hidden md:block">
             <Image src={signin} alt="signin" height={320} width={320} />
             <p className="text-[11px] text-black/[0.5] pl-5 mt-8">
               By signing up, you agree to our Terms & conditions, Privacy policy
