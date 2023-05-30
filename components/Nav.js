dd9565b64d7bb5de0c76b1349606ea385c006c2a
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-30 22:57:45 +0600

    user nav

--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { AiFillCaretDown } from "react-icons/ai";
 import { MdSearch } from "react-icons/md";
 import icon from "../public/icon.png";
+import user from "../public/user.png";
 
 export default function Nav() {
   return (
@@ -23,7 +24,7 @@ export default function Nav() {
             />
           </div>
         </div>
-        <div>
+        {/* <div>
           <p className="flex items-center gap-2 font-[700]">
             <span>Create account.</span>
             <span className="text-[#2F6CE5] ">It’s free!</span>
@@ -31,6 +32,14 @@ export default function Nav() {
               <AiFillCaretDown />
             </button>
           </p>
+        </div> */}
+
+        <div className="flex items-center gap-x-4">
+          <Image src={user} alt="user" width={36} height={36} />
+          <p>Siddharth Goyal</p>
+          <span>
+          <AiFillCaretDown/>
+          </span>
         </div>
       </div>
     </nav>
